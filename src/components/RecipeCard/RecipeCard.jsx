436848a0858696c9e2ed32399bfbe764d2373410
adiecr <adieassal@gmail.com>
2022-09-14 18:27:09 +0300

    added more functionality

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react' 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
 
 export default function RecipeCard({recipe}){
 
+  const navigate = useNavigate();
+  function handleSeeMore(){
+    navigate(`/recipe/${recipe._id}`);
+  }
 
 return (
     <Card>
@@ -16,7 +21,7 @@ return (
       <Card.Text>
         Difficulty: <strong>{recipe.difficulty}</strong>
       </Card.Text>
-      <Button>See More</Button>
+      <Button onClick={handleSeeMore}>See More</Button>
       </Card.Body>
      </Card>
     )
