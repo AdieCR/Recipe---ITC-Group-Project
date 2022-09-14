d28ec0fe4f260baa964de6d591677caddd6700a6
adiecr <adieassal@gmail.com>
2022-09-14 17:15:37 +0300

    fixed search

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -7,12 +7,16 @@ export default function RecipeCard({recipe}){
 
 return (
     <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Img variant="top" src={recipe.picture} />
     <Card.Body>
-      <Card.Title></Card.Title>
+      <Card.Title>{recipe.recipeTitle}</Card.Title>
       <Card.Text>
+        Category: <strong> {recipe.category}</strong>
       </Card.Text>
-      <Button>Go somewhere</Button>
+      <Card.Text>
+        Difficulty: <strong>{recipe.difficulty}</strong>
+      </Card.Text>
+      <Button>See More</Button>
       </Card.Body>
      </Card>
     )
